016a345e3b9fb594db69a70eb4c29534fdb9ec25
Manish Jaunjalkar <117558314+manishjaunjalkar@users.noreply.github.com>
2023-04-12 15:19:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	greet(){
+		console.log("Hello, my name is " + name + ", I am "+ age+ " years old." )
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	prototype:Person;
+	jobGreet(){
+		console.log("Hello, my name is " +Employee.name+ ", I am " + Employee.age+"years old, and my job title is "Employee.jobTitle
+	}
+	
+}
 
 // Do not change code below this line
 window.Person = Person;
